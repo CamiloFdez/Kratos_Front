2fa8458715b320424dd894b8d62fd8d0c31dfb41
Andres Jacobo <andres.sepulveda-s@mail.escuelaing.edu.co>
2025-03-16 17:10:45 -0500

    Agrego parte restante que faltaron en el commit anterior

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./Components/Auth/login";
@@ -14,14 +12,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
